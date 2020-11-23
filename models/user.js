363cfe907e3526ef8a59c001c667be110c70e4b8
Whitney Harden <wtharden320@gmail.com>
2020-11-23 13:58:41 -0500

    db and user model start

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const {DataTypes} = require('sequelize');
+const db = require('../db');
+
+const User = db.define('user', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+})
