2a2f29eac8a6e087d6784953a8da4f3aac249606
Sam Wolf <samwolf37@gmail.com>
2020-12-01 09:53:16 -0500

    for heroku

--- db.js
+++ db.js
@@ -1,4 +1,8 @@
 const {Sequelize} = require('sequelize');
-const db = new Sequelize(process.env.DB_CONNECTION_STRING);
+const db = new Sequelize(process.env.DATABASE_URL || 
+    `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/checklist`,
+    {
+    dialect: 'postgres',
+});
 
 module.exports = db;
